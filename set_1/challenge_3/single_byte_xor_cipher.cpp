f7da12ee9d602dc74e82550b42623a6f8723a287
Bridge-PC <giovanespace@gmail.com>
2023-09-15 15:19:24 -0300

    challenge_3 solved.     - Pequeno brute force de 'a' a 'z'. Depois de 'A' a 'Z'.

--- set_1/challenge_3/single_byte_xor_cipher.cpp
+++ set_1/challenge_3/single_byte_xor_cipher.cpp
@@ -1,26 +1,40 @@
 #include <iostream>
+#include <string.h>
 
-int hexCharToInt(char);
-char hexIntToChar(int);
+int* singleByteXor(char*, char);
+
+//LETRA X!
 
 int main(int argc, char* argv[]) {
+	char caractere = 'A';
+	while (caractere <= 'Z') {
+		int* output = singleByteXor(argv[1], caractere);
 
-	std::string strInput = argv[1];
-	char mascara = argv[2][0];
+		std::cout << "Letra: " << caractere << '\n';
+		for (int i = 0; i < strlen(argv[1])/2; i++) {
+			std::cout << (char)output[i];
+		}
 
-	int hexInput[strInput.length()/2];
-	int xorChars[strInput.length()/2];
+		std::cout << "\n\n";
+
+		delete[] output;
+		caractere++;
+	}
+
+	return 0;
+}
+
+int* singleByteXor(char* input, char byte_ascii) {
+
+	std::string strInput = input;
+	char mascara = byte_ascii;
+
+	int* xorChars = new int[strInput.length()/2];
 	std::string hexPair;
 	for (int i = 0; i < strInput.length()/2; i++) {
 		hexPair = strInput.substr(i*2, 2);
 		xorChars[i] = std::stoi(hexPair, nullptr, 16) xor mascara;
 	}
 
-	for (char a : xorChars) {
-        std::cout << a;
-    }
-
-	std::cout << std::endl;
-
-	return 0;
+	return xorChars;
 }
