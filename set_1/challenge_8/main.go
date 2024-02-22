8ef336af9158b88f032bc07b9678ea393a2af523
GiovanePS <giovanespace@gmail.com>
2024-02-21 21:42:40 -0300

    chall8 incompleto

--- set_1/challenge_8/main.go
+++ set_1/challenge_8/main.go
@@ -4,17 +4,45 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"reflect"
 )
 
 func main() {
 	data, err := os.ReadFile("./input.txt")
 	checkErr("reading file", err)
-	fmt.Printf("%v\n\n\n", data)
 
+	output := findECBEncryptedLine(data)
+	if output != -1 {
+		fmt.Printf("O texto da linha %d está encriptado por ECB.\n", output+1)
+	} else {
+		fmt.Println("Número não encontrado.")
+	}
+}
+
+func findECBEncryptedLine(data []byte) int {
 	splitData := bytes.Split(data, []byte("\n"))
+	sizeEachLineData := len(splitData[0])
+	contador := 0
+	bytesTested := make([]byte, sizeEachLineData)
+	for lineNumber, lineValue := range splitData {
+		for i := 0; i < sizeEachLineData; i += 16 {
+			copy(bytesTested[i:i+16], lineValue[i:i+16])
+			contador = 0
+			for n := 0; n < sizeEachLineData; n += 16 {
+				if reflect.DeepEqual(bytesTested[n:n+16], lineValue[n:n+16]) {
+					fmt.Println(string(bytesTested[n:n+16]), string(lineValue[n:n+16]))
+					contador += 1
+				}
+			}
+			if contador >= 2 {
+				return lineNumber
+			}
+		}
 
-	fmt.Println(splitData)
+		bytesTested = make([]byte, sizeEachLineData)
+	}
 
+	return -1
 }
 
 func checkErr(s string, e error) {
